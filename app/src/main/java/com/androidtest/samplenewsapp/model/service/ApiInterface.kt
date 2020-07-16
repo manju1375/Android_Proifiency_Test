01edc6c983adb01442a6e1fad2ccab5b4349889f
Manjunath D <manju1375@gmail.com>
2020-07-16 19:31:01 +0530

    Added basic unit test case. Not aware of kotlin test cases better still learning

--- app/src/main/java/com/androidtest/samplenewsapp/model/service/ApiInterface.kt
+++ app/src/main/java/com/androidtest/samplenewsapp/model/service/ApiInterface.kt
@@ -2,10 +2,26 @@ package com.androidtest.samplenewsapp.model.service
 
 import com.androidtest.samplenewsapp.model.CountryNewsModel
 import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 
 interface ApiInterface {
     @GET("s/2iodh4vg0eortkl/facts.json/")
     fun getCountryNews(): Call<CountryNewsModel>
+
+    // factory methods
+    companion object {
+        fun create(baseUrl: String): ApiInterface {
+            // here we set the base url of our API
+            val retrofit = Retrofit.Builder().baseUrl(baseUrl)
+                    // add the JSON dependency so we can handle json APIs
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            // here we pass a reference to our API interface
+            // and get back a concrete instance
+            return retrofit.create(ApiInterface::class.java)
+        }
+    }
 }
